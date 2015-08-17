5d52766041a0e57afabfbaee024016e04a4159f4
marvin nikijuluw & micah courey <student@epicodus.com>
2015-08-17 14:17:31 -0700

    this commit will add silex and twig to the app

--- app/app.php
+++ app/app.php
@@ -1,30 +1,50 @@
 <?php
   require_once __DIR__."/../vendor/autoload.php";
   require_once __DIR__."/../src/Task.php";
+  require_once __DIR__."/../src/Category.php";
 
   $app = new Silex\Application();
   $server = 'mysql:host=localhost;dbname=to_do';
-  $usernamae = 'root';
+  $username = 'root';
   $password = 'root';
-  $DB = new PDO($server, $username, $password); 
+  $DB = new PDO($server, $username, $password);
 
   $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
   ));
 
   $app->get("/", function() use ($app) {
-    return $app['twig']->render('task.html.twig', array('tasks' => Task::getAll())); //changed tasks.twig
+    return $app['twig']->render('index.html.twig', array('tasks' => Task::getAll())); //changed tasks.twig
   });
+
+$app->get ("/tasks", function() use ($app) {
+    return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+});
+
+$app->get("categories", function() use ($app) {
+    return $app['twig']->render('categories.html.twig', array('categories'=>Category::getAll()));
+});
+
+$app->post("/categories", function() use ($app) {
+        $category = new Category($_POST['name']);
+        $category->save();
+        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/delete_categories", function() use ($app) {
+        Category::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
 
 $app->post("/tasks", function() use ($app) {
   $task = new Task($_POST['description']);
   $task->save();
-  return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
+  return $app['twig']->render('tasks.html.twig', array('tasks' => $task));
 });
 
   $app->post("/delete_tasks", function() use ($app) {
     Task::deleteAll();
-    return $app['twig']->render('delete_tasks.html.twig');
+    return $app['twig']->render('index.html.twig');
 });
 
   return $app;
